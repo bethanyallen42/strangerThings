12591cb0b286fc1cda4d9e089919140c3d1c32c8
bethanyallen42 <bethanyallen42@gmail.com>
2022-07-07 22:08:44 -0600

    updated api call to be functional with a variety of api requests

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,29 @@ export const fetchPosts = async () => {
     console.log("Can't fetch posts");
   }
 };
+
+export const apiCall = async (url, method, token, body) => {
+  try {
+    const options = {
+      method: method ? method.toUpperCase() : "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+
+    if (token) {
+      options.headers["Authorization"] = `Bearer ${token}`;
+    }
+    const response = await fetch(`${BASE_URL}/${url}`, options);
+    const data = await response.json();
+
+    if (data.error) {
+      throw data.error;
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
-import { Home, Posts, Login, Register } from "./index";
+import { NavLink, Route } from "react-router-dom";
+import { Home, Posts, Account } from "./index";
+import "../styles.css";
+import { apiCall } from "../api";
 
 const App = () => {
   const BASE_URL =
@@ -10,34 +12,49 @@ const App = () => {
   const [token, setToken] = useState("");
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/posts`);
-        const postsInfo = await response.json();
-        console.log(postsInfo.data.posts);
-        setPosts(postsInfo.data.posts);
-      } catch (error) {
-        console.error(error);
-        console.log("Can't fetch posts");
-      }
-    };
-
-    fetchPosts();
+    (async () => {
+      const postInfo = await apiCall("posts");
+      setPosts(postInfo.data.posts);
+    })();
   }, []);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await fetch(`${BASE_URL}/posts`);
+  //       const postsInfo = await response.json();
+  //       console.log(postsInfo.data.posts);
+  //       setPosts(postsInfo.data.posts);
+  //     } catch (error) {
+  //       console.error(error);
+  //       console.log("Can't fetch posts");
+  //     }
+  //   };
+
+  //   fetchPosts();
+  // }, []);
 
   return (
     <main>
+      <nav>
+        <NavLink exact to="/" className="navLink" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/posts" className="navLink" activeClassName="active">
+          Posts
+        </NavLink>
+        <NavLink to="/account" className="navLink" activeClassName="active">
+          Account
+        </NavLink>
+      </nav>
       <Route exact path="/">
         <Home />
       </Route>
       <Route path="/posts">
         <Posts posts={posts} setPosts={setPosts} />
       </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/register">
-        <Register />
+      <Route path="/account">
+        <Account />
       </Route>
     </main>
   );
